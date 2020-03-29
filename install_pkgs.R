9c7f9fe6381fe044da171aedffc639ef615dbd54
David Earn <earn@math.mcmaster.ca>
2020-03-29 17:58:10 -0400

    i1 definition was missing

--- install_pkgs.R
+++ install_pkgs.R
@@ -1,6 +1,7 @@
 ## install all packages needed by epigrowthfit
 pkgs <- c("bbmle","emdbook","Hmisc", "Deriv", "lubridate", "zoo", "numDeriv",
           "Rdpack","testthat", "outbreaks","knitr")
+i1 <- installed.packages()
 pkgs <- pkgs[!pkgs %in% rownames(i1)]
 install.packages(pkgs)
 
