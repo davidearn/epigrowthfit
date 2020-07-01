a8fe4502da2426a41fecaa33801364024e80ec62
Ben Bolker <bbolker@gmail.com>
2020-06-30 22:53:41 -0400

    working on TMB writeup

--- sandbox/epigrowthfit_TMB.cpp
+++ sandbox/epigrowthfit_TMB.cpp
@@ -13,23 +13,21 @@ bool isNA(Type x){
   return R_IsNA(asDouble(x));
 }
 
-
-
 // https://kaskr.github.io/adcomp/_book/Tutorial.html
 template<class Type>
 Type objective_function<Type>::operator() ()
 {
   // Data
   DATA_VECTOR(t);      // times (includes step [-1])
-  DATA_VECTOR(x);     // counts
-  DATA_INTEGER(debug); // debug flag
+  DATA_VECTOR(x);      // counts
+  DATA_INTEGER(debug); // debug flag (1=print debugging statements)
   
   // Parameters  
   PARAMETER(log_thalf);    // log half-max time
   PARAMETER(log_K);        // log carrying capacity
   PARAMETER(log_r);        // log growth rate
   PARAMETER(log_p);        // log Richards shape parameter
-  PARAMETER(log_nb_disp);  // log NB dispersion parameter (FIXME: reparameterize?)
+  PARAMETER(log_nb_disp);  // log NB dispersion parameter
 
   vector<Type> cumcurve(t.size()); // includes starting time
   vector<Type> inccurve(x.size());
@@ -37,27 +35,29 @@ Type objective_function<Type>::operator() ()
   // Objective function
   Type jnll = 0;
 
-  // inverse-link functions
+  // inverse-link transform parameters
   Type thalf = exp(log_thalf);
   Type K = exp(log_K);
   Type r = exp(log_r);
   Type p = exp(log_p);
   Type nb_disp = exp(log_nb_disp); 
 
-  Type nb_var;
+  Type nb_var; // NB variance
   
   // parameter est
 
-  // std::cout << "start"  << std::endl;
   // std::cout << "thalf " << thalf << " K " << K << " r " << r << " p " << p << " nbdisp " << nb_disp << std::endl;
+
+  // first calculate cumulative curve at observed points
   for (int i = 0; i < t.size(); i++) {
-    cumcurve(i) = K/pow(1+exp(-r*p*(t(i)-thalf)),1/p);
-    // cumcurve(i) = K/(1+exp(-r*p*(t(i)-thalf)));
+    cumcurve(i) = K/pow(1+exp(-r*p*(t(i)-thalf)),1/p);  // Richards
   }
+
+  // difference cumulative curve to get expected incidence
   for (int i = 0; i < x.size()-1; i++) {
     inccurve(i) = cumcurve(i+1)-cumcurve(i);
     if (!isNA(x(i))) {
-      nb_var = inccurve(i)*(1+inccurve(i)/nb_disp);
+      nb_var = inccurve(i)*(1+inccurve(i)/nb_disp); // compute NB variance from mean, dispersion param
       jnll -= dnbinom2(x(i),inccurve(i),nb_var,1);
       if (debug==1) {
 	std::cout << i << " " << x(i) << " " << cumcurve(i) << " " <<
