dd804ae8f0982b914deb171ea1baed23e58acd83
Mikael Jagan <jaganmn@mcmaster.ca>
2020-10-19 09:07:44 -0400

    Expand ability to customize plots

--- R/egf_init-methods.R
+++ R/egf_init-methods.R
@@ -10,14 +10,45 @@
 #'   since `object$date[1]`. Missing values are not tolerated.
 #' @param inc One of `"cumulative"` and `"interval"`,
 #'   indicating a type of incidence to plot.
+#' @param add A logical scalar. If `TRUE`, then the fitting window
+#'   and predicted curve are added to the current graphics device,
+#'   and nothing else is done.
+#' @param annotate A logical scalar. If `TRUE`, then a legend and
+#'   a list of parameter values are added to the right margin.
+#'   Ignored if `add = TRUE`.
 #' @param tol A non-negative number used only if `inc = "interval"`.
-#'   `x$cases[i]` is highlighted if
-#'   `diff(x$time)[i] < (1-tol)*m`, or
-#'   `diff(x$time)[i] > (1+tol)*m`, where
-#'   `m = median(diff(x$time))`.
+#'   `x$init$cases[i]` is highlighted according to
+#'   `point_style2` if `diff(x$init$time)[i] < (1-tol)*m` and
+#'   `point_style3` if `diff(x$init$time)[i] > (1+tol)*m`,
+#'   where `m = median(diff(x$init$time))`. In both cases,
+#'   the value of `diff(x$init$time)[i]` is printed above the point.
 #'   Assign 0 to ensure that all deviations from `m` are highlighted.
 #'   Assign `Inf` to disable highlighting.
-#' @param ... Unused optional arguments.
+#' @param polygon_style A named list of arguments to
+#'   [`polygon()`][graphics::polygon()], affecting the appearance
+#'   of the fitting window. Currently, only `col` and `border` are
+#'   implemented.
+#' @param line_style A named list of arguments to
+#'   [`lines()`][graphics::lines()], affecting the appearance
+#'   of the predicted curve. Currently, only `lty`, `lwd`, and `col`
+#'   are implemented.
+#' @param point_style1 A named list of arguments to
+#'   [`points()`][graphics::points()], affecting the appearance
+#'   of the observed data. Currently, only `pch`, `col`, `bg` and `cex`
+#'   are implemented.
+#' @param point_style2,point_style3 Alternatives to `point_style1`
+#'   used for highlighting certain points when `inc = "interval"`.
+#'   See argument `tol`.
+#' @param text_style A named list of arguments to
+#'   [`text()`][graphics::text()], affecting the appearance
+#'   of text printed above highlighted points. See argument `tol`.
+#'   Currently, only `pos`, `offset`, `col`, `cex`, and `font`
+#'   are implemented.
+#' @param ... Optional arguments. Used only by the `plot` method
+#'   to specify graphical parameter. See [`plot()`][base::plot()]
+#'   and [`par()`][graphics::par()]. Currently, only `xlim`, `ylim`,
+#'   `xlab`, `ylab`, and `main` are implemented. Any additional
+#'   parameters will be ignored.
 #'
 #' @return
 #' The `print` method returns `x` invisibly.
@@ -47,34 +78,36 @@
 #' ## Plot elements
 #'
 #' The bottom axis measures the number of days since `x$date[1]`.
-#' The left axis measures interval or cumulative incidence
-#' (depending on `inc`) on a log scale. Zeros are plotted as if
-#' they were `10^(-0.2)`, and are therefore distinguished from
-#' nonzero counts, which are always at least 1.
+#' The left axis measures interval or cumulative incidence (depending
+#' on `inc`) on a log scale. Zeros are plotted as if they were `10^-0.2`,
+#' and are therefore distinguished from nonzero counts, which are always
+#' at least 1.
 #'
-#' Observed data, specified by `x$time` and either `x$cases` or
-#' `cumsum(x$cases)` (depending on `inc`), are plotted as points.
-#' `cases[i]` gives the number of cases observed between `time[i]`
-#' and `time[i+1]`, and `cumsum(cases)[i]` the number observed
-#' between `time[1]` and `time[i+1]`. Both are plotted at `time[i+1]`.
+#' Observed data, specified by `x$time` and either `x$cases`
+#' or `cumsum(x$cases)` (depending on `inc`), are plotted as
+#' points. `cases[i]` gives the number of cases observed between
+#' `time[i]` and `time[i+1]`, and `cumsum(cases)[i]` the number
+#' observed between `time[1]` and `time[i+1]`. Both are plotted
+#' at `time[i+1]`.
 #'
-#' The left and right endpoints of the fitting window, specified by
-#' indices `x$first` and `x$last`, are displayed as vertical lines
-#' at `time[first+1]` and `time[last+1]` (adding one since `first`
+#' The fitting window, specified by indices `x$first` and
+#' `x$last`, is displayed as a filled rectangle behind the
+#' other plot elements. The left and right boundaries occur at
+#' `time[first+1]` and `time[last+1]` (adding one since `first`
 #' and `last` index `cases`, and `length(time) = length(cases)+1`).
 #'
 #' The incidence curve predicted by initial parameter estimates
-#' `x$theta0` is plotted as a teal line on grid points
-#' `wgrid = seq(time[first+1], time[last+1], by = m)`,
-#' where `m = 1` for cumulative incidence and
-#' where `m = median(diff(time))` for interval incidence
-#' (to ensure that the curve has the correct scale; see below).
-#  The predicted curve is obtained with `predict(x, wgrid)`.
-#  The initial parameter estimates are printed at the bottom
-#  of the right margin.
+#' `x$theta0` is displayed as a line supported on grid points
+#' `wgrid = seq(time[first+1], time[last+1], by = m)`, where
+#' `m = 1` for cumulative incidence and `m = median(diff(time))`
+#' for interval incidence (to ensure that the curve has the
+#' correct scale; see below). The predicted curve is obtained
+#' with `predict(x, wgrid)`. If `add = FALSE` and `annotate = TRUE`,
+#' then the fitted parameter estimates `x$theta0` are printed
+#' at the bottom of the right margin.
 #'
-#' Careful interpretation of observed interval incidence is required
-#' if the plotted time series is not equally spaced, as `cases`
+#' Careful interpretation of interval incidence is required if
+#' the plotted time series is not equally spaced, because `cases`
 #' roughly scales with `diff(time)`. That is, certain observations
 #' may vary from the predicted curve not due to chance, but because
 #' they represent a count over fewer or more days than the typical
@@ -168,15 +201,35 @@ predict.egf_init <- function(object, time = object$time, ...) {
 #' @export
 #' @import graphics
 #' @importFrom stats median
-plot.egf_init <- function(x, inc = "cumulative", tol = 0, ...) {
+#' @importFrom scales alpha
+plot.egf_init <- function(x, inc = "cumulative",
+                          add = FALSE, annotate = TRUE, tol = 0,
+                          polygon_style = list(col = "#DDCC7740", border = NA),
+                          line_style = list(lty = 1, lwd = 3, col = "#44AA99"),
+                          point_style1 = list(pch = 21, col = "#BBBBBB", bg = "#DDDDDD", cex = 1),
+                          point_style2 = list(pch = 1, col = "#882255", bg = NA, cex = 1),
+                          point_style3 = list(pch = 16, col = "#882255", bg = NA, cex = 1),
+                          text_style = list(pos = 3, offset = 0.3, col = "#BBBBBB", cex = 0.7, font = 2),
+                          ...) {
   if (!is.character(inc) || length(inc) != 1 ||
       !inc %in% c("interval", "cumulative")) {
     stop("`inc` must be one of \"interval\", \"cumulative\".")
+  }
+  if (!is.logical(add) || length(add) != 1 || is.na(add)) {
+    stop("`add` must be `TRUE` or `FALSE`.")
+  }
+  if (!is.logical(annotate) || length(annotate) != 1 || is.na(annotate)) {
+    stop("`annotate` must be `TRUE` or `FALSE`.")
   }
   if (inc == "interval") {
     if (!is.numeric(tol) || length(tol) != 1 || !isTRUE(tol >= 0)) {
       stop("`tol` must be a non-negative number.")
     }
+  }
+  l <- list(polygon_style, line_style, text_style,
+            point_style1, point_style2, point_style3)
+  if (!all(sapply(l, is.list))) {
+    stop("All \"_style\" arguments must be lists.")
   }
 
 
@@ -204,9 +257,12 @@ plot.egf_init <- function(x, inc = "cumulative", tol = 0, ...) {
   varname <- paste0(varname, "_inc")
   formula <- as.formula(paste(varname, "~ time"))
 
-  ## Axis titles
-  xlab <- paste("days since", as.character(x$date[1]))
-  ylab <- paste(inc, "incidence")
+  ## Titles
+  xlab <- if ("xlab" %in% names(dots)) dots$xlab else paste("days since", as.character(x$date[1]))
+  ylab <- if ("ylab" %in% names(dots)) dots$ylab else paste(inc, "incidence")
+  cstr <- x$curve
+  substr(cstr, 1, 1) <- toupper(substr(cstr, 1, 1)) # capitalize first letter
+  main <- if ("main" %in% names(dots)) dots$main else paste(cstr, "model of", inc, "incidence\n(initial guess)")
 
   ## Axis limits (x)
   xmin <- 0
@@ -223,102 +279,124 @@ plot.egf_init <- function(x, inc = "cumulative", tol = 0, ...) {
   yaxis_at <- 10^(0:floor(log10(ymax)))
   yaxis_labels <- parse(text = paste0("10^", log10(yaxis_at)))
 
-  ## Point and line styles
-  points_bg_main <- "#DDDDDD"
-  points_bg_ltm <- "#FFFFFF"
-  points_bg_gtm <- "#882255"
-  points_col_main <- "#BBBBBB"
-  points_col_ltm <- "#882255"
-  points_col_gtm <- "#882255"
-  text_col <- "#BBBBBB"
-  lines_col <- "#44AA99"
-  if (inc == "cumulative") {
-    data$points_bg <- points_bg_main
-    data$points_col <- points_col_main
-  } else if (inc == "interval") {
-    data$dt <- diff(x$time)
-    m <- median(data$dt)
-    dt_min <- (1 - tol) * m
-    dt_max <- (1 + tol) * m
-    dt_enum <- 1 + 1 * (data$dt < dt_min) + 2 * (data$dt > dt_max)
-    data$points_bg <- c(points_bg_main, points_bg_ltm, points_bg_gtm)[dt_enum]
-    data$points_col <- c(points_col_main, points_col_ltm, points_col_gtm)[dt_enum]
+  ## Styles
+  for (a in grep("_style", names(formals(plot.egf_init)), value = TRUE)) {
+    l1 <- eval(formals(plot.egf_init)[[a]]) # default style
+    l2 <- get(a) # passed style
+    inter <- intersect(names(l1), names(l2))
+    l1[inter] <- l2[inter]
+    assign(a, l1)
   }
+
+  ## Style for each point
+  ## (for interval incidence, style depends on observation interval)
+  data$dt <- diff(x$time)
+  m <- median(data$dt)
+  dt_min <- (1 - tol) * m
+  dt_max <- (1 + tol) * m
+  data$style <- 1 +
+    (inc == "interval") * (1 * (data$dt < dt_min) + 2 * (data$dt > dt_max))
 
 
   ### PLOT #############################################################
 
-  op <- par(mar = c(5, 4, 4, 8) + 0.1, las = 1, mgp = c(3, 0.7, 0))
-  on.exit(par(op))
-  plot.new()
-  plot.window(xlim = xlim, ylim = ylim, xaxs = "i", yaxs = "i", log = "y")
+  if (!add) {
+    op <- par(mar = c(4, 4, 2, 7 * annotate) + 0.5,
+              mgp = c(3, 0.7, 0),
+              las = 1)
+    on.exit(par(op))
+    plot.new()
+    plot.window(xlim = xlim, ylim = ylim, xaxs = "i", yaxs = "i", log = "y")
+  }
 
-  ## Axes
-  box(bty = "l")
-  axis(side = 1)
-  axis(side = 2, at = yaxis_at, labels = yaxis_labels)
+  ## Fitting window
+  l <- list(
+    x = x$time[c(f+1, l+1, l+1, f+1)],
+    y = ylim[c(1, 1, 2, 2)]
+  )
+  do.call(polygon, c(l, polygon_style))
 
-  ## Observed data
-  xpd <- !any(c("xlim", "ylim") %in% names(dots))
-  points(formula, data = data, xpd = xpd,
-         pch = 21, bg = data$points_bg, col = data$points_col)
-  if (inc == "interval" && any(dt_enum != 1)) {
-    text(int_inc ~ time, data = data, subset = (dt_enum != 1),
-         labels = dt, pos = 3, offset = 0.3,
-         cex = 0.7, font = 2, col = text_col)
+  if (!add) {
+    ## Axes
+    box(bty = "l")
+    axis(side = 1)
+    axis(side = 2, at = yaxis_at, labels = yaxis_labels)
+
+    ## Observed data
+    for (i in 1:3) {
+      l <- list(
+        formula = formula,
+        data = data,
+        subset = (data$style == i),
+        xpd = !any(c("xlim", "ylim") %in% names(dots))
+      )
+      do.call(points, c(l, get(paste0("point_style", i))))
+    }
+
+    ## Annotation above exceptional points
+    if (any(data$style != 1)) {
+      l <- list(
+        formula = int_inc ~ time,
+        data = data,
+        subset = (data$style != 1)
+      )
+      do.call(text, c(l, text_style))
+    }
   }
 
   ## Predicted curve
-  lines(formula, data = wpred, lwd = 3, col = lines_col)
+  l <- list(
+    formula = formula,
+    data = wpred
+  )
+  do.call(lines, c(l, line_style))
 
-  ## Fitting window
-  abline(v = x$time[c(f,l)+1], lty = 2, col = "#555555")
+  if (!add) {
+    ## Titles
+    title(xlab = xlab)
+    title(ylab = ylab)
+    title(main = main, cex.main = 0.9)
 
-  ## Titles
-  title(xlab = xlab)
-  title(ylab = ylab)
-  cstr <- x$curve
-  substr(cstr, 1, 1) <- toupper(substr(cstr, 1, 1)) # capitalize first letter
-  title(main = paste(cstr, "model of", inc, "incidence\n(initial guess)"),
-        cex.main = 0.9)
+    if (annotate) {
+      ## Parameter estimates
+      pstr1 <- paste0(names(x$theta0), " = ")
+      pstr2 <- round(x$theta0, digits = 4)
+      if ("K" %in% names(x$theta0)) {
+        pstr2[["K"]] <- round(pstr2[["K"]])
+      }
+      px <- par("usr")[2] + 0.02 * diff(par("usr")[1:2])
+      px <- px + max(strwidth(pstr1, cex = 0.7))
+      py <- 10^(par("usr")[3] + 0.02 * diff(par("usr")[3:4]))
+      text(px, py, paste(pstr1, collapse = "\n"),
+           adj = c(1, 0), xpd = NA, cex = 0.7)
+      text(px, py, paste(pstr2, collapse = "\n"),
+           adj = c(0, 0), xpd = NA, cex = 0.7)
 
-  ## Parameter estimates
-  pstr1 <- paste0(names(x$theta0), " = ")
-  pstr2 <- round(x$theta0, digits = 4)
-  if ("K" %in% names(x$theta0)) {
-    pstr2[["K"]] <- round(pstr2[["K"]])
+      ## Legend (beware: some ugly hacks here)
+      lx <- par("usr")[2] + 0.02 * diff(par("usr")[1:2])
+      ly <- 10^(par("usr")[4] - 0.02 * diff(par("usr")[3:4]))
+      if (inc == "cumulative") {
+        lstr <- c("obs", NA, NA, "pred")
+        index <- c(TRUE, FALSE, FALSE, TRUE)
+      } else if (inc == "interval") {
+        lstr1 <- paste0("'", c("obs,", "obs,", "obs,", "pred,"), "'")
+        cond <- (all(data$dt[data$style == 1] == m))
+        rel <- c(if(cond) "=" else "~", "<", ">", "=")
+        mstr <- paste0(m, " day", if (m > 1) "s" else "")
+        lstr2 <- paste0("'t ", rel, " ", mstr, "'")
+        lstr <- parse(text = paste(lstr1, "~ Delta *", lstr2))
+        index <- c(TRUE, any(data$style == 2), any(data$style == 3), TRUE)
+      }
+      legend(x = lx, y = ly,
+             xpd = NA, bty = "n", cex = 0.7, seg.len = 1,
+             legend = lstr[index],
+             pch = c(point_style1$pch, point_style2$pch, point_style3$pch, NA)[index],
+             pt.bg = c(point_style1$bg, point_style2$bg, point_style3$bg, NA)[index],
+             lty = c(NA, NA, NA, line_style$lty)[index],
+             lwd = c(NA, NA, NA, line_style$lwd)[index],
+             col = c(point_style1$col, point_style2$col, point_style3$col, line_style$col)[index])
+    }
   }
-  px <- par("usr")[2] + 0.02 * diff(par("usr")[1:2])
-  px <- px + max(strwidth(pstr1, cex = 0.7))
-  py <- 10^(par("usr")[3] + 0.02 * diff(par("usr")[3:4]))
-  text(px, py, paste(pstr1, collapse = "\n"),
-       adj = c(1, 0), xpd = NA, cex = 0.7)
-  text(px, py, paste(pstr2, collapse = "\n"),
-       adj = c(0, 0), xpd = NA, cex = 0.7)
-
-  ## Legend (beware: many ugly hacks here)
-  lx <- par("usr")[2] + 0.02 * diff(par("usr")[1:2])
-  ly <- 10^(par("usr")[4] - 0.02 * diff(par("usr")[3:4]))
-  if (inc == "cumulative") {
-    lstr <- c("obs", NA, NA, "pred")
-    index <- c(TRUE, FALSE, FALSE, TRUE)
-  } else if (inc == "interval") {
-    lstr1 <- paste0("'", c("obs,", "obs,", "obs,", "pred,"), "'")
-    cond <- (all(data$dt[dt_enum == 1] == m))
-    rel <- c(if(cond) "=" else "~", "<", ">", "=")
-    mstr <- paste0(m, " day", if (m > 1) "s" else "")
-    lstr2 <- paste0("'t ", rel, " ", mstr, "'")
-    lstr <- parse(text = paste(lstr1, "~ Delta *", lstr2))
-    index <- c(TRUE, any(dt_enum == 2), any(dt_enum == 3), TRUE)
-  }
-  legend(x = lx, y = ly,
-         xpd = NA, bty = "n", cex = 0.7, seg.len = 1,
-         legend = lstr[index],
-         pch = c(21, 21, 21, NA)[index],
-         pt.bg = c(points_bg_main, points_bg_ltm, points_bg_gtm, NA)[index],
-         lty = c(NA, NA, NA, 1)[index],
-         lwd = c(NA, NA, NA, 3)[index],
-         col = c(points_col_main, points_col_ltm, points_col_gtm, lines_col)[index])
 
   invisible(NULL)
 }
