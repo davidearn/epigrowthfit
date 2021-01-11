ec415f106e00417c23e935776a5b615cbfd365ac
Mikael Jagan <jaganmn@mcmaster.ca>
2021-01-11 03:59:12 -0500

    Actually add package help page

---
+++ R/epigrowthfit-package.R
@@ -0,0 +1,21 @@
+#' \pkg{epigrowthfit}: Nonlinear Mixed Effects Models of Epidemic Growth
+#'
+#' @description
+#' A description of the \pkg{epigrowthfit} package.
+#'
+#' @details
+#' Details about the \pkg{epigrowthfit} package.
+#'
+#' @references
+#' \insertRef{Ma+14}{epigrowthfit}
+#'
+#' \insertRef{Earn+20}{epigrowthfit}
+#'
+#' @name epigrowthfit-package
+#' @docType package
+#' @keywords internal
+#' @importFrom Rdpack reprompt
+#' @importFrom mathjaxr preview_rd
+NULL
+
+.epigrowthfit <- new.env(parent = emptyenv())
