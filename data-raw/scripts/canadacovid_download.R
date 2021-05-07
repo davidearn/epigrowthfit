4b017f7bc7af4a445d436f23dbde9dda30013238
Mikael Jagan <jaganmn@mcmaster.ca>
2021-05-07 15:17:49 -0400

    Simplify raw data

--- data-raw/scripts/canadacovid_download.R
+++
@@ -1,6 +0,0 @@
-url <- "https://wzmli.github.io/COVID19-Canada/COVID19_Canada.csv"
-filename <- "../canadacovid_raw.csv"
-download.file(url, filename)
-canadacovid_raw <- read.csv(filename, header = TRUE)
-save(canadacovid_raw, file = sub(".csv", ".RData", filename))
-file.remove(filename)
