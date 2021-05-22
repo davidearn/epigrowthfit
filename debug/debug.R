6e82aba5a4a8b025d63153907b14dc5e595c97d8
Mikael Jagan <jaganmn@mcmaster.ca>
2021-05-22 14:17:18 -0400

    Include another attempted fix

--- debug/debug.R
+++ debug/debug.R
@@ -34,8 +34,9 @@ f <- function(x) { # dummy to contr.sum
 ## from fixed effects model
 Y_init <- matrix(numeric(0L), ncol = 4L)
 endpoints_split <- split(endpoints, endpoints$country_iso_alpha3)
+cia3 <- names(endpoints_split)
 for (i in seq_along(endpoints_split)) {
-  print(names(endpoints_split)[i])
+  print(cia3[i])
   object_1ts <- egf(
     formula_ts  = cases_new ~ Date | country_iso_alpha3,
     data_ts     = world,
@@ -51,17 +52,18 @@ for (i in seq_along(endpoints_split)) {
   object_1ts <- update(object_1ts, debug = FALSE, init = init)
   Y_init <- rbind(Y_init, matrix(fitted(object_1ts)$estimate, ncol = 4L))
 }
-m <- colMeans(Y_init)
-debug(epigrowthfit:::optim_tmb_out)
 
 ## Initial parameter vector for mixed effects model
+m <- colMeans(Y_init)
 beta <- c(f(Y_init[, 1L]), m[2:4])
 b <- Y_init[, 2:4] - rep(m[2:4], each = nrow(Y_init))
 log_sd_b <- log(apply(matrix(b, ncol = 3L), 2L, sd))
 #b <- rep_len(0, 3L * nrow(Y_init))
 #log_sd_b <- rep_len(1, 3L)
+init <- c(beta, b, log_sd_b)
 
 ## Mixed effects model fit
+debugonce(epigrowthfit:::optim_tmb_out)
 object <- egf(
   formula_ts  = cases_new ~ Date | country_iso_alpha3,
   data_ts     = world,
@@ -77,7 +79,42 @@ object <- egf(
   distr       = "nbinom",
   na_action   = c("exclude", "fail"),
   trace       = 1L,
-  init        = c(beta, b, log_sd_b)
+  init        = init
 )
-capture.output(tmb_out$fn(tmb_out$par), file = "debug.Rout")
+outfile <- file("debug.Rout", open = "wt")
+sink(outfile, type = "output")
+sink(outfile, type = "message")
+tmb_out$fn(tmb_out$par)
+sink(type = "output")
+sink(type = "message")
 Q
+
+
+
+
+o <- do.call(order, unname(endpoints[c("window", "country_iso_alpha3")]))
+b_nolex <- Y_init[o, 2:4] - rep(m[2:4], each = nrow(Y_init))
+init_nolex <- c(beta, b_nolex, log_sd_b)
+
+outfile_nolex <- file("debug_nolex.Rout", open = "wt")
+sink(outfile_nolex, type = "output")
+sink(outfile_nolex, type = "message")
+object <- egf(
+  formula_ts  = cases_new ~ Date | country_iso_alpha3,
+  data_ts     = world,
+  formula_par = list(
+    log(r)      ~ I(country_iso_alpha3:window),
+    log(tinfl)  ~ 1 | country_iso_alpha3:window,
+    log(K)      ~ 1 | country_iso_alpha3:window,
+    log(nbdisp) ~ 1 | country_iso_alpha3:window
+  ),
+  data_par    = endpoints,
+  endpoints   = endpoints,
+  curve       = "logistic",
+  distr       = "nbinom",
+  na_action   = c("exclude", "fail"),
+  trace       = 1L,
+  init        = init_nolex
+)
+sink(type = "output")
+sink(type = "message")
