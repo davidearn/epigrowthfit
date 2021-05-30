45389514423bc7345706689567691255e002a2da
Mikael Jagan <jaganmn@mcmaster.ca>
2021-05-30 13:10:03 -0400

    Add Makefile

--- mre/mre.R
+++ mre/mre.R
@@ -1,16 +1,12 @@
 # https://github.com/kaskr/adcomp/issues/348
 
 library("TMB")
-# packageVersion("TMB") # 1.7.20
+packageVersion("TMB") # 1.7.20
 
 zz <- "mre"
 compile(paste0(zz, ".cpp"))
 dyn.load(dynlib(zz))
 load(paste0(zz, ".RData")) # tmb_data, tmb_parameters
-
-# outfile <- file(paste0(zz, ".Rout"), open = "wt")
-# sink(outfile, type = "output")
-# sink(outfile, type = "message")
 
 obj <- MakeADFun(
   data = tmb_data,
@@ -23,6 +19,3 @@ obj <- MakeADFun(
 )
 opt <- with(obj, nlminb(par, fn, gr, control = list(trace = 1L)))
 sdr <- try(sdreport(obj))
-
-# sink(type = "message")
-# sink(type = "output")
