9b96ffbc0c47a46a5a1b288517312b54b3d3006c
Mikael Jagan <jaganmn@mcmaster.ca>
2021-09-02 04:40:38 -0400

    DRY refactor

--- tests/testthat/test-enums.R
+++ tests/testthat/test-enums.R
@@ -1,27 +1,16 @@
 test_that("egf_get_flag", {
-  flag_curve <- egf_get_flag("curve", c("exponential", "invalid name"))
-  expect_type(flag_curve, "integer")
-  expect_length(flag_curve, 2L)
-  expect_gte(flag_curve[[1L]], 0L)
-  expect_equal(flag_curve[[2L]], -1L)
-
-  flag_family <- egf_get_flag("family", c("pois", "invalid name"))
-  expect_type(flag_family, "integer")
-  expect_length(flag_family, 2L)
-  expect_gte(flag_family[[1L]], 0L)
-  expect_equal(flag_family[[2L]], -1L)
-
-  flag_prior <- egf_get_flag("prior", c("norm", "invalid name"))
-  expect_type(flag_prior, "integer")
-  expect_length(flag_prior, 2L)
-  expect_gte(flag_prior[[1L]], 0L)
-  expect_equal(flag_prior[[2L]], -1L)
-
-  flag_test <- egf_get_flag("test", c("logspace_diff", "invalid name"))
-  expect_type(flag_test, "integer")
-  expect_length(flag_test, 2L)
-  expect_gte(flag_test[[1L]], 0L)
-  expect_equal(flag_test[[2L]], -1L)
-
+  f <- function(type, enum) egf_get_flag(type, c(enum, "invalid name"))
+  flag <- Map(f,
+    type = c("curve", "family", "prior", "test"),
+    enum = c("exponential", "pois", "norm", "logspace_diff")
+  )
+  for (s in names(flag)) {
+    eval(bquote({
+      expect_type(flag[[.(s)]], "integer")
+      expect_length(flag[[.(s)]], 2L)
+      expect_gte(flag[[.(s)]][1L], 0L)
+      expect_equal(flag[[.(s)]][2L], -1L)
+    }))
+  }
   expect_error(egf_get_flag("invalid name", c("foo", "bar")))
 })
