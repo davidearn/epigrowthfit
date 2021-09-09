d23772e087d465b939c6ac4d555b8681513e04ef
Mikael Jagan <jaganmn@mcmaster.ca>
2021-09-09 19:17:55 -0400

    Improve 'print'

--- R/print.R
+++ R/print.R
@@ -1,15 +1,19 @@
 #' @export
-#' @importFrom utils str
-print.egf <- function(x, ...) {
+#' @importFrom utils str capture.output
+print.egf <- function(x, width = 0.9 * getOption("width"), exdent = 2L, ...) {
+  width <- as.integer(width)
+  extent <- strrep(" ", exdent)
+
+  ## Sort out details of top level nonlinear model
   mean <- "f(t) - f(s)"
   if (x$model$day_of_week > 0L) {
-    mean <- paste0("w(s, t) * ", mean)
+    mean <- paste("w(s, t) *", mean)
   }
   distribution <- switch(x$model$family,
     pois = sprintf("dpois(lambda = %s)", mean),
     nbinom = sprintf("dnbinom(mu = %s, size = disp)", mean)
   )
-  distribution <- paste0("X(s, t) ~ ", distribution)
+  distribution <- paste("X(s, t) ~", distribution)
   cumulative <- switch(x$model$curve,
     exponential = "c0 * exp(r * t)",
     subexponential = "c0 * (1 + alpha * (1 - p) * t / c0^(1 - p))^(1 / (1 - p))",
@@ -22,35 +26,53 @@ print.egf <- function(x, ...) {
   }
   cumulative <- paste("f(t) =", cumulative)
 
-  width <- as.integer(0.9 * getOption("width"))
-  cat(wrap("This is an 'egf' object. See '?egf' for class details.", width = width), "\n")
-  cat("\n")
-  h <- "Top level nonlinear model"
-  cat(h, strrep(".", max(0L, width - nchar(h))), "\n")
-  cat("\n")
-  cat(" ", distribution, "\n")
-  cat(" ", "where\n")
-  cat(" ", cumulative, "\n")
-  cat("\n")
-  str(x$model, no.list = TRUE, indent.str = "  ")
-  cat("\n")
-  h <- "Bottom level mixed effects model"
-  cat(h, strrep(".", max(0L, width - nchar(h))), "\n")
-  cat("\n")
-  f <- function(x, s) deparse1(call("~", str2lang(s), formula(terms(x))[[2L]]))
-  bottom <- mapply(f, x$frame_parameters, names(x$frame_parameters))
+  ## Utilities
+  cat0 <- function(...) {
+    cat(..., sep = "")
+  }
+  heading <- function(s) {
+    cat0(s, " ", strrep(".", max(0L, width - 1L - nchar(s))), "\n")
+  }
+  pluralize <- function(s, n) {
+    if (n > 1L) paste0(s, "s") else s
+  }
+  get_formula_string <- function(frame, parameter, indent) {
+    cl <- call("~", str2lang(parameter), formula(terms(frame))[[2L]])
+    paste0(indent, capture.output(print(cl)), collapse = "\n")
+  }
+
+  cat0(wrap("This is an 'egf' object. See '?egf' for class documentation.", width = width), "\n")
+  cat0("\n")
+
+  heading("Top level nonlinear model")
+  cat0("\n")
+  cat0(exdent, distribution, "\n")
+  cat0(exdent, "where\n")
+  cat0(exdent, cumulative, "\n")
+  cat0("\n")
+  str(x$model, no.list = TRUE, indent.str = exdent)
+  cat0("\n")
+
+  heading("Bottom level mixed effects model")
+  cat0("\n")
   w <- nchar(names(x$frame_parameters))
-  cat(paste(strrep(" ", 1L + max(w) - w), bottom, "\n", collapse = ""))
-  cat("\n")
-  h <- "Data"
-  cat(h, strrep(".", max(0L, width - nchar(h))), "\n")
-  cat("\n")
+  formula_string <- mapply(get_formula_string,
+    frame = x$frame_parameters,
+    parameter = names(x$frame_parameters),
+    indent = paste0(exdent, strrep(" ", max(w) - w))
+  )
+  cat0(paste0(formula_string, "\n", collapse = ""))
+  cat0("\n")
+
+  heading("Data")
+  cat0("\n")
   n <- sum(!is.na(x$frame$window))
   N <- nlevels(x$frame$window)
   M <- nlevels(x$frame$ts)
   w <- 1L + as.integer(log10(max(n, N, M)))
-  cat(sprintf("%*d top level observation%s\n", 2L + w, n, if (n > 0L) "s" else ""))
-  cat(sprintf("%*d bottom level observation%s (fitting window%s)\n", 2L + w, N, if (N > 0L) "s" else "", if (N > 0L) "s" else ""))
-  cat(sprintf("%*d time series\n", 2L + w, M))
+  cat0(exdent, sprintf("%*d top level %s\n", w, n, pluralize("observation", n)))
+  cat0(exdent, sprintf("%*d bottom level %s (fitting %s)\n", w, N, pluralize("observation", N), pluralize("window", N)))
+  cat0(exdent, sprintf("%*d time series\n", w, M))
+
   invisible(x)
 }
