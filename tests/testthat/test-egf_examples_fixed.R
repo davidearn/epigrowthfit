6d9b4e5d198358a6d1b400c3cfdbacad8c0360e0
Mikael Jagan <jaganmn@mcmaster.ca>
2021-09-24 11:47:13 -0400

    Reset test tolerances [do check]

--- tests/testthat/test-egf_examples_fixed.R
+++ tests/testthat/test-egf_examples_fixed.R
@@ -1,28 +1,26 @@
 test_that("exponential", {
   r <- log(2) / 20
   c0 <- 100
-  disp <- 50
 
-  zz <- simulate(egf_model(curve = "exponential", family = "nbinom"),
+  zz <- simulate(egf_model(curve = "exponential", family = "pois"),
     nsim = 1L,
     seed = 412575L,
-    mu = log(c(r, c0, disp)),
+    mu = log(c(r, c0)),
     cstart = 10
   )
   mm <- egf(zz)
-  expect_equal(mm$best, zz$actual, tolerance = 1e-1)
+  expect_equal(mm$best, zz$actual, tolerance = 5e-2)
 })
 
 test_that("subexponential", {
   alpha <- log(2) / 20
   c0 <- 100
   p <- 0.95
-  disp <- 50
 
-  zz <- simulate(egf_model(curve = "subexponential", family = "nbinom"),
+  zz <- simulate(egf_model(curve = "subexponential", family = "pois"),
     nsim = 1L,
     seed = 696182L,
-    mu = c(log(alpha), log(c0), qlogis(p), log(disp)),
+    mu = c(log(alpha), log(c0), qlogis(p)),
     cstart = 10
   )
   mm <- egf(zz,
@@ -37,12 +35,11 @@ test_that("gompertz", {
   alpha <- log(2) / 20
   tinfl <- 100
   K <- 25000
-  disp <- 50
 
-  zz <- simulate(egf_model(curve = "gompertz", family = "nbinom"),
+  zz <- simulate(egf_model(curve = "gompertz", family = "pois"),
     nsim = 1L,
     seed = 720748L,
-    mu = log(c(alpha, tinfl, K, disp)),
+    mu = log(c(alpha, tinfl, K)),
     cstart = 10
   )
   mm <- egf(zz)
@@ -53,12 +50,11 @@ test_that("logistic", {
   r <- log(2) / 20
   tinfl <- 100
   K <- 25000
-  disp <- 50
 
-  zz <- simulate(egf_model(curve = "logistic", family = "nbinom"),
+  zz <- simulate(egf_model(curve = "logistic", family = "pois"),
     nsim = 1L,
     seed = 366465L,
-    mu = log(c(r, tinfl, K, disp)),
+    mu = log(c(r, tinfl, K)),
     cstart = 10
   )
   mm <- egf(zz)
@@ -70,12 +66,11 @@ test_that("richards", {
   tinfl <- 100
   K <- 25000
   a <- 1.005
-  disp <- 50
 
-  zz <- simulate(egf_model(curve = "richards", family = "nbinom"),
+  zz <- simulate(egf_model(curve = "richards", family = "pois"),
     nsim = 1L,
     seed = 51520L,
-    mu = log(c(r, tinfl, K, a, disp)),
+    mu = log(c(r, tinfl, K, a)),
     cstart = 10
   )
   mm <- egf(zz,
--- tests/testthat/test-egf_examples_random.R
+++ tests/testthat/test-egf_examples_random.R
@@ -3,13 +3,15 @@ oo <- options(egf.cores = 4L)
 test_that("exponential", {
   r <- log(2) / 20
   c0 <- 100
-  disp <- 50
 
-  zz <- simulate(egf_model(curve = "exponential", family = "nbinom"),
-    nsim = 100L,
+  mu <- log(c(r, c0))
+  Sigma <- diag(rep_len(0.2^2, length(mu)))
+
+  zz <- simulate(egf_model(curve = "exponential", family = "pois"),
+    nsim = 20L,
     seed = 775494L,
-    mu = log(c(r, c0, disp)),
-    Sigma = diag(rep_len(0.2^2, 3L)),
+    mu = mu,
+    Sigma = Sigma,
     cstart = 10
   )
   mm <- egf(zz,
@@ -17,53 +19,59 @@ test_that("exponential", {
       Sigma ~ LKJ(eta = 2)
     )
   )
-  expect_lt(max(abs(mm$gradient)), 5e-3)
+
+  expect_lt(max(abs(mm$gradient)), 5e-5)
   pp <- split(data.frame(actual = zz$actual, fitted = mm$best),
               sub("\\[[0-9]+\\]$", "", names(mm$best)))
-  expect_equal(pp$beta$fitted, pp$beta$actual, tolerance = 2e-2)
-  expect_equal(pp$theta$fitted[1:3], pp$theta$actual[1:3], tolerance = 2e-2)
-  expect_equal(pp$theta$fitted[-(1:3)], pp$theta$actual[-(1:3)], tolerance = 5e-1)
+  expect_equal(pp$beta$fitted, pp$beta$actual, tolerance = 5e-2)
+  expect_equal(pp$theta$fitted[seq_along(mu)], pp$theta$actual[seq_along(mu)], tolerance = 2e-1)
+  expect_equal(pp$theta$fitted[-seq_along(mu)], pp$theta$actual[-seq_along(mu)], tolerance = 5e-1)
 })
 
 test_that("subexponential", {
   alpha <- log(2) / 20
   c0 <- 100
   p <- 0.95
-  disp <- 50
 
-  zz <- simulate(egf_model(curve = "subexponential", family = "nbinom"),
-    nsim = 100L,
+  mu <- c(log(alpha), log(c0), qlogis(p))
+  Sigma <- diag(rep_len(0.2^2, length(mu)))
+
+  zz <- simulate(egf_model(curve = "subexponential", family = "pois"),
+    nsim = 20L,
     seed = 653927L,
-    mu = c(log(alpha), log(c0), qlogis(p), log(disp)),
-    Sigma = diag(rep_len(0.2^2, 4L)),
+    mu = mu,
+    Sigma = Sigma,
     cstart = 10
   )
   mm <- egf(zz,
     formula_priors_bottom = list(
-      beta[3L] ~ Normal(mu = qlogis(p), sigma = 0.05),
-      theta[3L] ~ Normal(mu = log(0.2), sigma = 0.25),
+      beta[3L] ~ Normal(mu = mu[3L], sigma = 0.05),
+      theta[3L] ~ Normal(mu = 0.5 * log(Sigma[3L, 3L]), sigma = 0.25),
       Sigma ~ LKJ(eta = 2)
     )
   )
-  expect_lt(max(abs(mm$gradient)), 5e-3)
+
+  expect_lt(max(abs(mm$gradient)), 5e-4)
   pp <- split(data.frame(actual = zz$actual, fitted = mm$best),
               sub("\\[[0-9]+\\]$", "", names(zz$actual)))
-  expect_equal(pp$beta$fitted, pp$beta$actual, tolerance = 2e-2)
-  expect_equal(pp$theta$fitted[1:4], pp$theta$actual[1:4], tolerance = 1e-1)
-  expect_equal(pp$theta$fitted[-(1:4)], pp$theta$actual[-(1:4)], tolerance = 1e-1)
+  expect_equal(pp$beta$fitted, pp$beta$actual, tolerance = 5e-2)
+  expect_equal(pp$theta$fitted[seq_along(mu)], pp$theta$actual[seq_along(mu)], tolerance = 2e-1)
+  expect_equal(pp$theta$fitted[-seq_along(mu)], pp$theta$actual[-seq_along(mu)], tolerance = 5e-1)
 })
 
 test_that("gompertz", {
   alpha <- log(2) / 20
   tinfl <- 100
   K <- 25000
-  disp <- 50
 
-  zz <- simulate(egf_model(curve = "gompertz", family = "nbinom"),
-    nsim = 100L,
+  mu <- log(c(alpha, tinfl, K))
+  Sigma <- diag(rep_len(0.2^2, length(mu)))
+
+  zz <- simulate(egf_model(curve = "gompertz", family = "pois"),
+    nsim = 20L,
     seed = 685399L,
-    mu = log(c(alpha, tinfl, K, disp)),
-    Sigma = diag(rep_len(0.2^2, 4L)),
+    mu = mu,
+    Sigma = Sigma,
     cstart = 10
   )
   mm <- egf(zz,
@@ -71,25 +79,28 @@ test_that("gompertz", {
       Sigma ~ LKJ(eta = 2)
     )
   )
-  expect_lt(max(abs(mm$gradient)), 5e-2)
+
+  expect_lt(max(abs(mm$gradient)), 5e-4)
   pp <- split(data.frame(actual = zz$actual, fitted = mm$best),
               sub("\\[[0-9]+\\]$", "", names(zz$actual)))
   expect_equal(pp$beta$fitted, pp$beta$actual, tolerance = 5e-2)
-  expect_equal(pp$theta$fitted[1:4], pp$theta$actual[1:4], tolerance = 2e-2)
-  expect_equal(pp$theta$fitted[-(1:4)], pp$theta$actual[-(1:4)], tolerance = 2)
+  expect_equal(pp$theta$fitted[seq_along(mu)], pp$theta$actual[seq_along(mu)], tolerance = 2e-1)
+  expect_equal(pp$theta$fitted[-seq_along(mu)], pp$theta$actual[-seq_along(mu)], tolerance = 2)
 })
 
 test_that("logistic", {
   r <- log(2) / 20
   tinfl <- 100
   K <- 25000
-  disp <- 50
 
-  zz <- simulate(egf_model(curve = "logistic", family = "nbinom"),
-    nsim = 100L,
+  mu <- log(c(r, tinfl, K))
+  Sigma <- diag(rep_len(0.2^2, length(mu)))
+
+  zz <- simulate(egf_model(curve = "logistic", family = "pois"),
+    nsim = 20L,
     seed = 397981L,
-    mu = log(c(r, tinfl, K, disp)),
-    Sigma = diag(rep_len(0.2^2, 4L)),
+    mu = mu,
+    Sigma = Sigma,
     cstart = 10
   )
   mm <- egf(zz,
@@ -97,12 +108,13 @@ test_that("logistic", {
       Sigma ~ LKJ(eta = 2)
     )
   )
-  expect_lt(max(abs(mm$gradient)), 5e-2)
+
+  expect_lt(max(abs(mm$gradient)), 1e-2)
   pp <- split(data.frame(actual = zz$actual, fitted = mm$best),
               sub("\\[[0-9]+\\]$", "", names(zz$actual)))
-  expect_equal(pp$beta$fitted, pp$beta$actual, tolerance = 5e-3)
-  expect_equal(pp$theta$fitted[1:4], pp$theta$actual[1:4], tolerance = 1e-1)
-  expect_equal(pp$theta$fitted[-(1:4)], pp$theta$actual[-(1:4)], tolerance = 2e-1)
+  expect_equal(pp$beta$fitted, pp$beta$actual, tolerance = 1e-2)
+  expect_equal(pp$theta$fitted[seq_along(mu)], pp$theta$actual[seq_along(mu)], tolerance = 5e-2)
+  expect_equal(pp$theta$fitted[-seq_along(mu)], pp$theta$actual[-seq_along(mu)], tolerance = 1e-1)
 })
 
 test_that("richards", {
@@ -110,13 +122,15 @@ test_that("richards", {
   tinfl <- 100
   K <- 25000
   a <- 1.005
-  disp <- 10
 
-  zz <- simulate(egf_model(curve = "richards", family = "nbinom"),
-    nsim = 100L,
+  mu <- log(c(r, tinfl, K, a))
+  Sigma <- diag(rep_len(0.2^2, length(mu)))
+
+  zz <- simulate(egf_model(curve = "richards", family = "pois"),
+    nsim = 20L,
     seed = 949642L,
-    mu = log(c(r, tinfl, K, a, disp)),
-    Sigma = diag(rep_len(0.2^2, 5L)),
+    mu = mu,
+    Sigma = Sigma,
     cstart = 10
   )
   mm <- egf(zz,
@@ -126,12 +140,12 @@ test_that("richards", {
       Sigma ~ LKJ(eta = 2)
     )
   )
-  expect_lt(max(abs(mm$gradient)), 1e-1)
+  expect_lt(max(abs(mm$gradient)), 2e-2)
   pp <- split(data.frame(actual = zz$actual, fitted = mm$best),
               sub("\\[[0-9]+\\]$", "", names(zz$actual)))
   expect_equal(pp$beta$fitted, pp$beta$actual, tolerance = 5e-3)
-  expect_equal(pp$theta$fitted[1:5], pp$theta$actual[1:5], tolerance = 1e-1)
-  expect_equal(pp$theta$fitted[-(1:5)], pp$theta$actual[-(1:5)], tolerance = 5e-1)
+  expect_equal(pp$theta$fitted[seq_along(mu)], pp$theta$actual[seq_along(mu)], tolerance = 1e-1)
+  expect_equal(pp$theta$fitted[-seq_along(mu)], pp$theta$actual[-seq_along(mu)], tolerance = 2e-1)
 })
 
 options(oo)
