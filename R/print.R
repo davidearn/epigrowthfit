da34c96bf03c32758b706369c63f3e3230a17bec
Mikael Jagan <jaganmn@mcmaster.ca>
2021-09-28 03:39:04 -0400

    Avoid 'print.data.frame'; compute summaries safely

--- R/print.R
+++ R/print.R
@@ -4,16 +4,19 @@ print.egf <- function(x, width = 0.9 * getOption("width"), indent = 2L, ...) {
   width <- as.integer(width)
   indent <- strrep(" ", indent)
 
-  mean <- "f(t) - f(s)"
+  ## Set up ===========================================================
+
+  ## Top level nonlinear model
+  top_mean <- "f(t) - f(s)"
   if (x$model$day_of_week > 0L) {
-    mean <- paste("w(s, t) *", mean)
+    top_mean <- paste("w(s, t) *", top_mean)
   }
-  distribution <- switch(x$model$family,
-    pois = sprintf("dpois(lambda = %s)", mean),
-    nbinom = sprintf("dnbinom(mu = %s, size = disp)", mean)
+  top_distribution <- switch(x$model$family,
+    pois = sprintf("dpois(lambda = %s)", top_mean),
+    nbinom = sprintf("dnbinom(mu = %s, size = disp)", top_mean)
   )
-  distribution <- paste("X(s, t) ~", distribution)
-  cumulative <- switch(x$model$curve,
+  top_distribution <- paste("X(s, t) ~", top_distribution)
+  top_cumulative <- switch(x$model$curve,
     exponential = "c0 * exp(r * t)",
     subexponential = "c0 * (1 + alpha * (1 - p) * t / c0^(1 - p))^(1 / (1 - p))",
     gompertz = "K * exp(-exp(-alpha * (t - tinfl)))",
@@ -21,50 +24,59 @@ print.egf <- function(x, width = 0.9 * getOption("width"), indent = 2L, ...) {
     richards = "K / (1 + a * exp(-a * r * (t - tinfl)))^(1 / a)"
   )
   if (x$model$excess) {
-    cumulative <- paste("b * t +", cumulative)
+    top_cumulative <- paste0("b * t + ", top_cumulative)
   }
-  cumulative <- paste("f(t) =", cumulative)
+  top_cumulative <- paste0("f(t) = ", top_cumulative)
+  top_lines <- paste0(indent, c(top_distribution, "where", top_cumulative))
+
+  ## Bottom level mixed effects model
+  f <- function(frame, parameter, indent) {
+    cl <- call("~", str2lang(parameter), formula(terms(frame))[[2L]])
+    lines <- capture.output(print(cl))
+    paste0(indent, lines)
+  }
+  nch <- nchar(names(x$frame_parameters))
+  offset <- strrep(" ", max(nch) - nch)
+  bottom_lines <- unlist(Map(f,
+    frame = x$frame_parameters,
+    parameter = names(x$frame_parameters),
+    indent = paste0(indent, offset)
+  ))
+
+  ## Data
+  n <- c(
+    sum(!is.na(x$frame$window)),
+    nlevels(x$frame$window),
+    nlevels(x$frame$ts)
+  )
+  ltext <- format(sprintf("%d", n), justify = "right")
+  rtext <- c(
+    sprintf("top level %s (%s)", pluralize("observation", n[1L]), pluralize("count", n[1L])),
+    sprintf("bottom level %s (fitting %s)", pluralize("observation", n[2L]), pluralize("window", n[2L])),
+    "time series"
+  )
+  data_lines <- paste0(indent, ltext, " ", rtext)
+
+  ## Print ============================================================
 
   cat(wrap("This is an 'egf' object. See '?egf' for class documentation.", width = width), "\n", sep = "")
   cat("\n")
 
   heading("Top level nonlinear model", width = width)
   cat("\n")
-  writeLines(paste0(indent, c(distribution, "where", cumulative)))
+  writeLines(top_lines)
   cat("\n")
   str(x$model, no.list = TRUE, indent.str = indent, give.head = FALSE)
   cat("\n")
 
   heading("Bottom level mixed effects model", width = width)
   cat("\n")
-  get_formula_lines <- function(frame, parameter, indent) {
-    cl <- call("~", str2lang(parameter), formula(terms(frame))[[2L]])
-    lines <- capture.output(print(cl))
-    paste0(indent, lines)
-  }
-  w <- nchar(names(x$frame_parameters))
-  writeLines(unlist(Map(get_formula_lines,
-    frame = x$frame_parameters,
-    parameter = names(x$frame_parameters),
-    indent = paste0(indent, strrep(" ", max(w) - w))
-  )))
+  writeLines(bottom_lines)
   cat("\n")
 
   heading("Data", width = width)
   cat("\n")
-  counts <- c(
-    n <- sum(!is.na(x$frame$window)),
-    N <- nlevels(x$frame$window),
-    nlevels(x$frame$ts)
-  )
-  left <- sprintf("%d", counts)
-  right <- c(
-    sprintf("top level %s (%s)", pluralize("observation", n), pluralize("count", n)),
-    sprintf("bottom level %s (fitting %s)", pluralize("observation", N), pluralize("window", N)),
-    "time series"
-  )
-  w <- nchar(left)
-  writeLines(paste0(indent, strrep(" ", max(w) - w), left, " ", right))
+  writeLines(data_lines)
 
   invisible(x)
 }
--- R/summary.R
+++ R/summary.R
@@ -1,13 +1,24 @@
 #' @export
 #' @importFrom stats fitted aggregate
 summary.egf <- function(object, ...) {
+  safe_summary <- function(x) {
+    res <- unclass(summary(x))
+    if (anyNA(x)) {
+      return(res)
+    }
+    c(res, `NA's` = 0)
+  }
   fo <- fitted(object)
-  sfo <- aggregate(fo["estimate"], fo["top"], summary)
+  sfo <- aggregate(fo["estimate"], fo["top"], safe_summary)
+  sfo <- `colnames<-`(t(sfo[[2L]]), as.character(sfo[[1L]]))
+  if (sum(sfo[7L, ] == 0)) {
+    sfo <- sfo[-7L, , drop = FALSE]
+  }
   res <- list(
     convergence = object$optimizer_out$convergence,
     value = object$value,
     gradient = object$gradient,
-    fitted = `colnames<-`(t(sfo[[2L]]), as.character(sfo[[1L]]))
+    fitted = sfo
   )
   class(res) <- c("egf_summary", "list")
   res
@@ -18,33 +29,17 @@ print.egf_summary <- function(x, width = 0.9 * getOption("width"), indent = 2L, 
   width <- as.integer(width)
   indent <- strrep(" ", indent)
 
-  vrag <- sprintf("%.6e", c(x$value, range(abs(x$gradient))))
-  dd <- data.frame(
-    c(
-      "convergence",
-      "value",
-      "range(abs(gradient))"
-    ),
-    c(
-      sprintf("%d", x$convergence),
-      vrag[1L],
-      paste(vrag[2L], vrag[3L])
-    ),
-    fix.empty.names = FALSE,
-    stringsAsFactors = FALSE
-  )
-
   heading("Negative log likelihood", width = width)
-  lines <- capture.output(print(dd, right = FALSE, quote = FALSE, row.names = FALSE, max = 6L))
-  lines <- paste0(indent, sub("^.", "", lines))
-  writeLines(lines)
+  cat("\n")
+  vrag <- sprintf("%.6e", c(x$value, range(abs(x$gradient))))
+  ltext <- c("convergence", "value", "range(abs(gradient))")
+  rtext <- c(sprintf("%d", x$convergence), vrag[1L], paste(vrag[2L], vrag[3L]))
+  writeLines(paste0(indent, format(ltext, justify = "left"), " ", rtext))
   cat("\n")
 
   heading("Fitted values", width = width)
   cat("\n")
-  lines <- capture.output(print(x$fitted))
-  lines <- paste0(indent, lines)
-  writeLines(lines)
+  writeLines(paste0(indent, capture.output(print(x$fitted))))
 
   invisible(x)
 }
