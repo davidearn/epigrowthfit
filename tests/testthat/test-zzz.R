7f7a42df394da1e2c08a87c066a4bb147ef40959
Mikael Jagan <jaganmn@mcmaster.ca>
2024-01-09 09:44:53 -0500

    disable load time test for version mismatch

--- tests/testthat/test-zzz.R
+++ tests/testthat/test-zzz.R
@@ -10,33 +10,3 @@ test_that(".on(Load|Unload)", {
     expect_true(is_ns_loaded())
     expect_true(is_so_loaded())
 })
-
-test_that("version mismatch", {
-    path_to_build_versions <-
-        system.file("build_versions", package = "epigrowthfit", mustWork = TRUE)
-    expect_true(nzchar(path_to_build_versions))
-
-    build_versions <- readLines(path_to_build_versions)
-    expect_gt(length(build_versions), 0L)
-    expect_true(all(grepl("^[[:alnum:].]{2,} [[:digit:]]+(\\.[[:digit:]]+)+$",
-                          build_versions)))
-    expect_true(all(vapply(strsplit(build_versions, " "), `[[`, "", 1L) %in%
-                    rownames(installed.packages())))
-
-    v <- packageVersion("TMB")
-    mmp <- unclass(v)[[1L]]
-    vm <- as.package_version("0.0.0")
-    vp <- as.package_version(as.numeric_version(list(c(mmp[1:2], mmp[3L]+1L))))
-
-    test_versions <- paste("TMB", c(v, vm, vp))
-    regexp <- list(NA, NULL, NULL)
-    unloadNamespace("epigrowthfit")
-    for (i in 1:3) {
-        writeLines(test_versions[[i]], path_to_build_versions)
-        eval(bquote(expect_warning(loadNamespace("epigrowthfit"),
-                                   regexp = .(regexp[[i]]))))
-        unloadNamespace("epigrowthfit")
-    }
-    writeLines(build_versions, path_to_build_versions)
-    loadNamespace("epigrowthfit")
-})
