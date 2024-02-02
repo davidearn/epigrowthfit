887777badb0f86cf5fc64e06480161b1a9b6cfb0
Mikael Jagan <jaganmn@mcmaster.ca>
2024-02-02 13:31:12 -0500

    clean up; incl. fix for non-integral cases of Dceiling(<double>)

--- R/utils_datetime.R
+++ R/utils_datetime.R
@@ -3,43 +3,45 @@
 ##' Extracts year, month, and day (in UTC) from a date-time object.
 ##'
 ##' @param x
-##'   A \link{POSIXlt}, \link{POSIXct}, or \link{Date} object.
+##'   a \link{Date}, \link{POSIXct}, or \link{POSIXlt} object.
 ##' @param which
-##'   A character string composed of the letters \samp{y m d},
+##'   a character string composed of the letters \samp{y m d},
 ##'   indicating which of (and in what order) year, month, and day
 ##'   should be returned.
 ##' @param drop
-##'   A logical scalar. If \code{drop = TRUE}, then the matrix result
+##'   a logical.  If \code{drop = TRUE}, then the matrix result
 ##'   is coerced to a vector when one of its dimensions is less than 2.
 ##'
 ##' @return
 ##' \code{X[, j, drop = drop]}, where \code{X} is an \link{integer}
 ##' \link{matrix} with \code{length(x)} rows and 3 columns listing
-##' year, month, and day, and \code{j} is an index vector derived
-##' from \code{which}.
+##' year, month, and day, and \code{j} is the index vector specified
+##' by \code{which}.
 ##'
 ##' @examples
 ##' x <- .Date(sample.int(1e+04L, 10L))
 ##' ymd(x)
-##' ymd(x, which = "d", drop = TRUE)
+##' ymd(x, which = "d")
 
 ymd <-
 function(x, which = "ymd", drop = TRUE) {
-	stopifnot(inherits(x, c("Date", "POSIXlt", "POSIXct")),
-	          is.character(which),
-	          length(which) == 1L,
-	          !is.na(which),
-	          is.logical(drop),
-	          length(drop) == 1L,
-	          !is.na(drop))
+	stopifnot(exprs = {
+		inherits(x, c("Date", "POSIXct", "POSIXlt"))
+		is.character(which)
+		length(which) == 1L
+		!is.na(which)
+		is.logical(drop)
+		length(drop) == 1L
+		!is.na(drop)
+	})
 	x <- as.POSIXct(x)
 	attr(x, "tzone") <- "UTC"
 	x <- as.POSIXlt(x)
-	res <- matrix(c(1900L + x$year, 1L + x$mon, x$mday),
-	              nrow = length(x), ncol = 3L,
-	              dimnames = list(names(x), c("y", "m", "d")))
-	j <- match(strsplit(which, "")[[1L]], colnames(res), 0L)
-	res[, j, drop = drop]
+	ans <- c(1900L + x$year, 1L + x$mon, x$mday)
+	dim(ans) <- c(length(x), 3L)
+	dimnames(ans) <- list(names(x), c("y", "m", "d"))
+	j <- match(strsplit(which, "")[[1L]], colnames(ans), 0L)
+	ans[, j, drop = drop]
 }
 
 ##' Round a Date vector
@@ -49,8 +51,8 @@ function(x, which = "ymd", drop = TRUE) {
 ##' midnight on a first-of-the-month (\code{YYYY-MM-01}), or
 ##' midnight on a first-of-the-year (\code{YYYY-01-01}).
 ##'
-##' @param x A \link{Date} vector.
-##' @param to A \link{character} string.
+##' @param x a \link{Date} vector.
+##' @param to a \link{character} string.
 ##'
 ##' @return
 ##' \code{x} with elements rounded.
@@ -64,22 +66,22 @@ function(x, which = "ymd", drop = TRUE) {
 ##' ceiling_x <- data.frame(unclass_x = unclass(x), x = x, l)
 ##'
 ##' l <- sapply(dmy, Dfloor, x = x, simplify = FALSE)
-##' floor_x <- replace(ceiling_y, dmy, l)
+##' floor_x <- replace(ceiling_x, dmy, l)
 
 Dceiling <-
 function(x, to = c("day", "month", "year")) {
 	stopifnot(inherits(x, "Date"))
 	to <- match.arg(to)
-	if (to == "day") {
+	if (to == "day")
 		return(.Date(ceiling(unclass(x))))
-	}
 	x <- as.POSIXlt(x)
+	off <- x$mday > 1L | x$hour > 0L | x$min > 0L | x$sec > 0
 	if (to == "month") {
-		x$mon <- x$mon + (x$mday > 1L)
-		x$year <- x$year + (i <- x$mon > 11L)
-		x$mon[i] <- 0L
+		x$mon <- x$mon + off
+		x$year <- x$year + (off <- x$mon > 11L)
+		x$mon[off] <- 0L
 	} else {
-		x$year <- x$year + (x$mon > 0L || x$mday > 1L)
+		x$year <- x$year + (x$mon > 0L | off)
 		x$mon[] <- 0L
 	}
 	x$mday[] <- 1L
@@ -90,13 +92,11 @@ Dfloor <-
 function(x, to = c("day", "month", "year")) {
 	stopifnot(inherits(x, "Date"))
 	to <- match.arg(to)
-	if (to == "day") {
+	if (to == "day")
 		return(.Date(floor(unclass(x))))
-	}
 	x <- as.POSIXlt(x)
 	x$mday[] <- 1L
-	if (to == "year") {
+	if (to == "year")
 		x$mon[] <- 0L
-	}
 	as.Date(x)
 }
